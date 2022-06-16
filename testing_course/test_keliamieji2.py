96aac2666ac28a1a700ca8ecf0c85c70a658ed1f
Vytautas Sluckas <vytautas.sluckas@exacaster.com>
2022-06-16 18:23:22 +0300

    testing course

---
+++ testing_course/test_keliamieji2.py
@@ -0,0 +1,13 @@
+import unittest
+from keliamieji2 import ar_keliamieji2
+
+
+class TestKeliamieji2(unittest.TestCase):
+    def test_ar_keliamieji(self):
+        self.assertTrue(ar_keliamieji2(2000))
+        self.assertTrue(ar_keliamieji2(2020))
+        self.assertFalse(ar_keliamieji2(2100))
+
+
+if __name__ == "__main__":
+    unittest.main()
