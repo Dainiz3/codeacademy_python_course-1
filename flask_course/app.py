2b23066dbb40f2aa931b9a09b3278b6f36ba8703
Vytautas Sluckas <vytautas.sluckas@exacaster.com>
2022-06-16 21:59:19 +0300

    learning flask

---
+++ flask_course/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    print(request)
+    print(request.form)
+    if request.method == "POST":
+        vardas = request.form["vardas"]
+        return render_template("greetings.html", vardas=vardas)
+    else:
+        return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run()
