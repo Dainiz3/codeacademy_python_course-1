7b6f61d4682c88277f15acc5c174ae09319c9057
Vytautas Sluckas <vytautas.sluckas@exacaster.com>
2022-07-21 18:05:48 +0300

    flask introduction

---
+++ flask_advanced/testing.py
@@ -0,0 +1,3 @@
+from flask import render_template
+
+print(render_template("index.html"))
---
+++ flask_advanced/testing2.py
@@ -0,0 +1,7 @@
+print("starting import")
+from testing import a
+print("finishing import")
+
+print(a)
+print(f"I am in testing2 {__name__}")
+
