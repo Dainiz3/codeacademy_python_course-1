33e9c836b85a9d2298469d72523bd229047a57cf
Vytautas Sluckas <vytautas.sluckas@exacaster.com>
2022-07-27 18:13:22 +0300

    flask part 1

--- flask_advanced/testing.py
+++ flask_advanced/testing.py
@@ -1,3 +1,10 @@
-from flask import render_template
+from articles.dictionary import data
 
-print(render_template("index.html"))
+
+new_list = []
+for item in data:
+    if item['status'] == 'published':
+        new_list.append(item)
+
+print(new_list)
+print(len(new_list))
--- flask_advanced/testing2.py
+++
@@ -1,7 +0,0 @@
-print("starting import")
-from testing import a
-print("finishing import")
-
-print(a)
-print(f"I am in testing2 {__name__}")
-
